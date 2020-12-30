bae87feefe1e0debc7573851bbd7c3abd4735c9a
Артём Валеев <artem19129@gmail.com>
2020-12-30 21:42:27 +0300

    optimization

--- src/app/ui/material/material.service.ts
+++ src/app/ui/material/material.service.ts
@@ -6,6 +6,10 @@ const svgIcons = [
   { name: 'category_d', path: 'images/driving-license/categories/category_d.svg' },
   { name: 'category_fly', path: 'images/driving-license/categories/category_fly.svg' },
   { name: 'category_m', path: 'images/driving-license/categories/category_m.svg' },
+  { name: 'exit', path: 'svg/icons/exit.svg' },
+  { name: 'star', path: 'svg/icons/star.svg' },
+  { name: 'usa_logo_eagle', path: 'svg/icons/usa_logo_eagle.svg' },
+  { name: 'right-line', path: 'svg/icons/right-line.svg' },
 ];
 
 @Injectable({
